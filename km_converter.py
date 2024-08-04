4324dc731a3c3e9f96d7440465b307b344d1740e
Carrie Mackenzie <carriemack89@icloud.com>
2024-08-04 16:52:26 +0100

    Update km_converter.py

--- km_converter.py
+++ km_converter.py
@@ -1,10 +1,14 @@
+#function to convert miles to kilometers
 def miles_to_km(miles):
     return miles * 1.60934
 
-
+#main part of the program (code inside this block will excute - when script is run directly)
 if __name__ == "__main__":
+    #prompt user to enter value (in this case the distance in miles)
     miles = float(input("Please enter the distance in miles: "))
 
+    #convert miles to km
     kilometers = miles_to_km(miles)
-
+    
+    #print the result
     print(f"{miles} miles is equal to {kilometers:.2f} Kilometers.")
